e0ce41a8e3d18fdcf183271e9e0b7f48565bf987
pallado1 <dominik.palla@uhk.cz>
2024-11-27 14:50:40 +0100

    Security module, User entity

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
@@ -9,6 +10,24 @@ public class MainController {
     @GetMapping("/")
     public String main(){
         return "main";
+    }
+
+    @GetMapping("/403")
+    @ResponseBody
+    public String forbidden(){
+        return "<h1>Access Denied</h1>";
+    }
+
+    @GetMapping("/admin")
+    @ResponseBody
+    public String admin(){
+        return "<h1>Admin Section</h1>";
+    }
+
+    @GetMapping("/blog")
+    @ResponseBody
+    public String blog(){
+        return "<h1>Blog Content</h1>";
     }
 
 }
